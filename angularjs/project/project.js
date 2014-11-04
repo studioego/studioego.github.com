cb7afa2f623a396977d1228d6541f48b4731bf11
DaeHyun Sung <sungdh86+git@gmail.com>
2014-11-04 16:41:09 +0900

    change file
    change file

--- angularjs/project/project.js
+++ angularjs/project/project.js
@@ -1,11 +1,11 @@
 angular.module('project', ['ngRoute', 'firebase'])
-
+ 
 .value('fbURL', 'https://angularjs-projects.firebaseio.com/')
-
+ 
 .factory('Projects', function($firebase, fbURL) {
   return $firebase(new Firebase(fbURL)).$asArray();
 })
-
+ 
 .config(function($routeProvider) {
   $routeProvider
     .when('/', {
@@ -24,11 +24,11 @@ angular.module('project', ['ngRoute', 'firebase'])
       redirectTo:'/'
     });
 })
-
+ 
 .controller('ListCtrl', function($scope, Projects) {
   $scope.projects = Projects;
 })
-
+ 
 .controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
   $scope.save = function() {
       Projects.$add($scope.project).then(function(data) {
@@ -36,28 +36,7 @@ angular.module('project', ['ngRoute', 'firebase'])
       });
   };
 })
-
-.controller('EditCtrl',
-  function($scope, $location, $routeParams, Projects) {
-    var projectId = $routeParams.projectId,
-        projectIndex;
-
-    $scope.projects = Projects;
-    projectIndex = $scope.projects.$indexFor(projectId);
-    $scope.project = $scope.projects[projectIndex];
-
-    $scope.destroy = function() {
-        $scope.projects.$remove($scope.project).then(function(data) {
-            $location.path('/');
-        });
-    };
-
-    $scope.save = function() {
-        $scope.projects.$save($scope.project).then(function(data) {
-           $location.path('/');
-        });
-    };
-})
+ 
 .controller('EditCtrl',
   function($scope, $location, $routeParams, Projects) {
     var projectId = $routeParams.projectId,
